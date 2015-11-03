c58c82c490df16d48626b33933f48a8a0d01d845
Joe Constant <joe@joeconstant.com>
2015-11-03 11:25:18 -0700

    Added Gen3 version of classes

---
+++ src/Gen3/RepliconService.php
@@ -0,0 +1,185 @@
+<?php namespace Constant\Replicon\Gen3;
+
+use Constant\Service\BaseSoapService;
+use Psr\Log\LoggerInterface;
+
+class RepliconService extends BaseSoapService
+{
+    protected $_baseUri = '';
+
+    /**
+     * Initialize class
+     * @param LoggerInterface $log
+     * @param $username
+     * @param $password
+     * @param array $options
+     */
+    public function __construct(LoggerInterface $log, $username, $password, $options = [])
+    {
+        $this->output = $log;
+        $companyKey = $options['companyKey'];
+        // $this->_baseUri = "https://na6.replicon.com/{$companyKey}/services/";
+        $this->_baseUri = "https://na6-replicon-com-37y852527011.runscope.net/{$companyKey}/services/";
+        $this->username = $username;
+        $this->password = $password;
+        // $this->_debug = true;
+        // $this->_options['proxy_host'] = '127.0.0.1';
+        // $this->_options['proxy_port'] = '8888';
+        $this->_options['login'] = "{$companyKey}\\{$this->username}";
+        $this->_options['password'] = $this->password;
+
+        $this->_options['trace'] = 1;
+        $this->_options['exceptions'] = true;
+        $this->_options['cache_wsdl'] = WSDL_CACHE_NONE;
+
+        $this->_options['stream_context'] = stream_context_create([
+            'http' => [
+                'protocol_version' => '1.0'
+            ],
+            'ssl' => [
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true
+            ]
+        ]);
+
+    }
+
+    public function getTimesheetById($timesheetid)
+    {
+        $data = [
+            "GetStandardTimesheet2" => [
+                "timesheetUri" => $timesheetid,
+            ]
+        ];
+
+        $this->output->debug(print_r($data, true));
+
+        $this->uri = $this->_baseUri . 'TimesheetService1.svc';
+        $this->wsdl = $this->uri . '?singleWsdl';
+        $this->_options['location'] = $this->uri . '/soap';
+        $response = $this->processRequest('GetStandardTimesheet2', $data);
+        $this->_handleError($data, $response);
+        return $response->GetStandardTimesheet2Result;
+    }
+
+    public function getTimesheetByUseridDate($userid, $date = '')
+    {
+        if (empty($date)) {
+            $date = date('Y-m-d', time() - 86400);
+        }
+        $date = explode('-', $date);
+
+        $data = [
+            "GetTimesheetForDate2" => [
+                "userUri" => $userid,
+                "date" => [
+                    "year" => $date[0],
+                    "month" => $date[1],
+                    "day" => $date[2]
+                ],
+                "timesheetGetOptionUri" => null
+            ]
+        ];
+
+        $this->output->debug(print_r($data, true));
+
+        $this->uri = $this->_baseUri . 'TimesheetService1.svc';
+        $this->wsdl = $this->uri . '?singleWsdl';
+        $this->_options['location'] = $this->uri . '/soap';
+        $response = $this->processRequest('GetTimesheetForDate2', $data);
+        $this->_handleError($data, $response);
+        return $response->GetTimesheetForDate2Result->timesheet->uri;
+    }
+
+    public function getTaskByCode($code)
+    {
+        $data = array(
+            'Action' => 'Query',
+            'QueryType' => 'TaskByCode',
+            'DomainType' => 'Replicon.Project.Domain.Task',
+            'Args' => array(
+                $code
+            )
+        );
+        $this->output->debug(print_r($data, true));
+        $data = json_encode($data);
+
+        $response = $this->processRequest($data);
+        $this->_handleError($data, $response);
+        return $response->Value[0]->Properties;
+    }
+
+    public function findUseridByLogin($username)
+    {
+
+        $data = [
+            'GetUser2' => [
+                'user' => [
+                    'uri' => null,
+                    'loginName' => $username,
+                    'parameterCorrelationId' => null
+                ]
+            ]
+        ];
+        $this->output->debug(print_r($data, true));
+
+        $this->uri = $this->_baseUri . 'UserService1.svc';
+        $this->wsdl = $this->uri . '?singleWsdl';
+        $this->_options['location'] = $this->uri . '/soap';
+        $response = $this->processRequest('GetUser2', $data);
+        $this->_handleError($data, $response);
+        $user = new \stdClass;
+        $user->Id = $response->GetUser2Result->uri;
+        return $user;
+    }
+
+    public function addTimeEntry($timesheet, $date, $code, $duration, $comment)
+    {
+        $date = explode('-', $date);
+        $data = [
+            "Action" => "Edit",
+            "Type" => "Replicon.Suite.Domain.EntryTimesheet",
+            "Identity" => (string)$timesheet,
+            "Operations" => [
+                [
+                    "__operation" => "CollectionAdd",
+                    "Collection" => "TimeEntries",
+                    "Operations" => [
+                        [
+                            "__operation" => "SetProperties",
+                            "CalculationModeObject" => [
+                                "Type" => "Replicon.TimeSheet.Domain.CalculationModeObject",
+                                "Identity" => "CalculateInOutTime",
+                                "Properties" => [
+                                    "Name" => "CalculationModeObject_CalculateInOutTime"
+                                ]
+                            ],
+                            "EntryDate" => [
+                                "__type" => "Date",
+                                "Year" => $date[0],
+                                "Month" => $date[1],
+                                "Day" => $date[2]
+                            ],
+                            "Duration" => [
+                                "__type" => "Timespan",
+                                "Hours" => $duration
+                            ],
+                            "Comments" => $comment,
+                            "Task" => [
+                                "Identity" => (string)$code
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ];
+
+        $this->output->debug(print_r($data, true));
+        $data = json_encode($data);
+
+        $response = $this->processRequest($data);
+        $this->_handleError($data, $response);
+    }
+
+}
---
+++ src/Gen3/Timesheet.php
@@ -0,0 +1,280 @@
+<?php namespace Constant\Replicon\Gen3;
+
+use Constant\Service\BaseSoapService;
+use Psr\Log\LoggerInterface;
+
+class Timesheet extends BaseSoapService
+{
+
+    private $id;
+
+    const ACTIVITY_NONE = 0;
+    const ACTIVITY_TRAINING = 4;
+    const ACTIVITY_VACATION = 6;
+    const ACTIVITY_SICK = 7;
+    const ACTIVITY_HOLIDAY = 8;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    protected $_timeRows = [];
+
+    /**
+     * Initialize class
+     * @param LoggerInterface $log
+     * @param $username
+     * @param $password
+     * @param array $options
+     */
+    public function __construct(LoggerInterface $log, $username, $password, $options = [])
+    {
+        $this->output = $log;
+        $companyKey = $options['companyKey'];
+        // $this->_baseUri = "https://na6.replicon.com/{$companyKey}/services/";
+        $this->_baseUri = "https://na6-replicon-com-37y852527011.runscope.net/{$companyKey}/services/";
+        $this->username = $username;
+        $this->password = $password;
+        // $this->_debug = true;
+        // $this->_options['proxy_host'] = '127.0.0.1';
+        // $this->_options['proxy_port'] = '8888';
+        $this->_options['login'] = "{$companyKey}\\{$this->username}";
+        $this->_options['password'] = $this->password;
+
+        $this->_options['trace'] = 1;
+        $this->_options['exceptions'] = true;
+        $this->_options['cache_wsdl'] = WSDL_CACHE_NONE;
+
+        $this->_options['stream_context'] = stream_context_create([
+            'http' => [
+                'protocol_version' => '1.0'
+            ],
+            'ssl' => [
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true
+            ]
+        ]);
+
+        $this->_timeRows = [];
+    }
+
+    public function saveTimesheet()
+    {
+        $data = [
+            'PutStandardTimesheet2' => [
+                'timesheet' => [
+                    'target' => [
+                        'uri' => "{$this->id}",
+                        'user' => null,
+                        'date' => null
+                    ],
+                    'customFields' => [],
+                    'rows' => $this->_timeRows,
+                    'noticeExplicitlyAccepted' => false,
+                    'bankedTime' => null
+                ]
+            ]
+        ];
+
+        $this->output->debug(print_r($data, true));
+
+        $this->uri = $this->_baseUri . 'TimesheetService1.svc';
+        $this->wsdl = $this->uri . '?singleWsdl';
+        $this->_options['location'] = $this->uri . '/soap';
+        $response = $this->processRequest('PutStandardTimesheet2', $data);
+        $this->_handleError($data, $response);
+        return $response;
+    }
+
+    public function createCell($date, $duration, $comment)
+    {
+        $hours = $duration;
+        $minutes = 0;
+        if (floor($duration) != $duration) {
+            $hours = floor($duration);
+            $minutes = ($duration - $hours) * 60;
+        }
+        $date = explode('-', $date);
+        return [
+            'date' => [
+                'year' => $date[0],
+                'month' => $date[1],
+                'day' => $date[2]
+            ],
+            'duration' => [
+                'hours' => $hours,
+                'minutes' => $minutes,
+                'seconds' => '0'
+            ],
+            'comments' => $comment,
+            'customFieldValues' => []
+        ];
+    }
+
+    public function addTimeRow($cells = [], $project = '', $task ='')
+    {
+        if (empty($project) || empty($task)) {
+            throw new \Exception("Project and Task are required.  Maybe you meant to use Gen2 class?");
+        }
+        $this->_timeRows[] = [
+            'target' => null,
+            'project' => $project,
+            'task' => $task,
+            'billingRate' => [
+                'displayText' => 'Project Rate',
+                'name' => 'Project Rate',
+                'uri' => 'urn:replicon:project-specific-billing-rate'
+            ],
+            'activity' => null,
+            'customFieldValues' => [],
+            'cells' => $cells
+        ];
+    }
+
+    public function createProject($taskCode) {
+
+        $projecturn = $this->_getProjectUrn($taskCode);
+        return [
+            'uri' => $projecturn,
+            'name' => null,
+            'parameterCorrelationId' => null
+        ];
+    }
+
+    public function createTask($taskCode)
+    {
+        $taskurn = $this->_getTaskUrn($taskCode);
+        return [
+            'uri' => $taskurn,
+            'name' => null,
+            'parent' => null,
+            'parameterCorrelationId' => null
+        ];
+    }
+
+    protected $_projects = [];
+    protected $_tasks = [];
+
+    public function getTask($taskCode)
+    {
+        return $this->_getTaskUrn($taskCode);
+    }
+
+    public function getProject($taskCode)
+    {
+        return $this->_getProjectUrn($taskCode);
+    }
+
+    protected function _getProjectUrn($taskCode)
+    {
+        if (empty($this->_projects)) {
+            $data = [
+                'GetPageOfProjectsAvailableForTimeAllocationFilteredByClientAndTextSearch' => [
+                    'page' => '1',
+                    'pageSize' => '10000',
+                    'timesheetUri' => $this->id,
+                    'clientUri' => null,
+                    'textSearch' => null,
+                    'clientNullFilterBehaviorUri' => null
+                ]
+            ];
+
+            $this->output->debug(print_r($data, true));
+
+            $this->uri = $this->_baseUri . 'TimesheetService1.svc';
+            $this->wsdl = $this->uri . '?singleWsdl';
+            $this->_options['location'] = $this->uri . '/soap';
+            $response = $this->processRequest('GetPageOfProjectsAvailableForTimeAllocationFilteredByClientAndTextSearch', $data);
+            $this->_handleError($data, $response);
+            foreach ($response->GetPageOfProjectsAvailableForTimeAllocationFilteredByClientAndTextSearchResult->TimeAllocationAvailableProjectDetails1 as $project) {
+                $tasks = $this->_getTasks($project->project->uri);
+                foreach ($tasks as $code => $taskuri) {
+                    $this->_projects[$code] = $project->project->uri;
+                }
+            }
+        }
+        if (isset($this->_projects[$taskCode])) {
+            return $this->_projects[$taskCode];
+        }
+
+        throw new \Exception("Could not find project for {$taskCode}");
+    }
+
+    protected function _getTasks($projectUrn)
+    {
+        if (isset($this->_tasks[$projectUrn])) {
+            return $this->_tasks[$projectUrn];
+        }
+
+        $data = [
+            'GetPageOfTasksAvailableForTimeAllocationFilteredByProjectAndTextSearch' => [
+                'page' => '1',
+                'pageSize' => '1000',
+                'timesheetUri' => $this->id,
+                'projectUri' => $projectUrn,
+                'textSearch' => null
+            ]
+        ];
+        $this->output->debug(print_r($data, true));
+
+        $this->uri = $this->_baseUri . 'TimesheetService1.svc';
+        $this->wsdl = $this->uri . '?singleWsdl';
+        $this->_options['location'] = $this->uri . '/soap';
+        $response = $this->processRequest('GetPageOfTasksAvailableForTimeAllocationFilteredByProjectAndTextSearch', $data);
+        $this->_handleError($data, $response);
+        if (!is_array($response->GetPageOfTasksAvailableForTimeAllocationFilteredByProjectAndTextSearchResult->TimeAllocationAvailableTaskDetails1)) {
+            $value = $response->GetPageOfTasksAvailableForTimeAllocationFilteredByProjectAndTextSearchResult->TimeAllocationAvailableTaskDetails1;
+            $response->GetPageOfTasksAvailableForTimeAllocationFilteredByProjectAndTextSearchResult->TimeAllocationAvailableTaskDetails1 = [$value];
+        }
+        foreach ($response->GetPageOfTasksAvailableForTimeAllocationFilteredByProjectAndTextSearchResult->TimeAllocationAvailableTaskDetails1 as $task) {
+            $taskCode = explode(' - ', $task->task->task->displayText);
+            $this->_tasks[$projectUrn][trim($taskCode[1])] = $task->task->task->uri;
+        }        
+        if (isset($this->_tasks[$projectUrn])) {
+            return $this->_tasks[$projectUrn];
+        }
+
+        throw new \Exception("Could not find tasks for {$projectUrn}");
+    }
+
+    protected function _getTaskUrn($taskCode)
+    {
+        $projecturi = $this->_getProjectUrn($taskCode);
+        if (!isset($this->_tasks[$projecturi])) {
+            return '';
+        }
+        if (!isset($this->_tasks[$projecturi][$taskCode])) {
+            return '';
+        }
+        return $this->_tasks[$projecturi][$taskCode];
+    }
+
+    public function createActivity($type = ACTIVITY_NONE)
+    {
+        throw new \Exception("Activities have not been implemented yet");
+    }
+
+    public function isGen2()
+    {
+        return false;
+    }
+
+    public function isGen3()
+    {
+        return true;
+    }
+
+}
